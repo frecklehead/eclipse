bc1af66416779ada8ed1805756e5b2f846699c3b
frecklehead <prashamsa35np@gmail.com>
2023-10-05 16:54:55 +0545

    first release

---
+++ script.js
@@ -0,0 +1,108 @@
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains_behind = document.getElementById('mountains_behind');
+let text = document.getElementById('text');
+let btn = document.getElementById('btn');
+let mountains_front = document.getElementById('mountains_front');
+
+
+window.addEventListener('scroll', function () {
+    let value = window.scrollY;
+    stars.style.left = value * 0.3 + 'px';
+    moon.style.top = value * 1.03 + 'px';
+    mountains_behind.style.top = value * 0.5 + 'px';
+    mountains_front.style.top = value * 0 + 'px';
+    text.style.marginRight = value * 3 + 'px';
+    text.style.marginTop = value * 1.5 + 'px';
+    btn.style.marginTop = value * 1.5 + 'px';
+
+})
+
+//book
+const prevBtn=document.getElementById("prev-btn");
+const nextBtn=document.getElementById("next-btn");
+
+const book=document.getElementById("book");
+
+const paper1=document.getElementById("p1")
+const paper2=document.getElementById("p2")
+const paper3=document.getElementById("p3")
+// event
+prevBtn.addEventListener("click",goPrevPage);
+nextBtn.addEventListener("click",goNextPage);
+
+
+
+let currentLocation=1;
+let numOfPapers=3;
+let maxLocation=numOfPapers + 1;
+
+function openBook(){
+    book.style.transform="translateX(50%)";
+    prevBtn.style.transform="translateX(-250px)";
+    nextBtn.style.transform="translateX(250px)";
+
+}
+
+function closeBook(isAtBeginning){
+   if(isAtBeginning) {
+    book.style.transform="translateX(0%)";
+   }else{
+    book.style.transform="translateX(100%)";
+   }
+   
+
+}
+
+function goNextPage(){
+ if(currentLocation<maxLocation){
+    switch(currentLocation){
+        case 1:
+            openBook();
+            paper1.classList.add("flipped");
+            paper1.style.zIndex=1;
+            break;
+        case 2:
+            paper2.classList.add("flipped");
+            paper2.style.zIndex=2;
+            break;
+        case 3:
+            paper3.classList.add("flipped");
+            paper3.style.zIndex=3;
+            closeBook(false);
+            break;
+        default:
+            throw new Error("unknown state");
+
+
+    }
+    currentLocation++;
+ }
+}
+
+function goPrevPage(){
+    if (currentLocation>1){
+        switch(currentLocation){
+            case 2:
+                closeBook(true);
+                paper1.classList.remove("flipped");
+                paper1.style.zIndex=3;
+                break;
+            case 3:
+                paper2.classList.remove("flipped");
+                paper2.style.zIndex=2;
+                break;
+            case 4:
+                openBook();
+                paper3.classList.remove("flipped");
+                paper3.style.zIndex=1;
+                break;
+            default:
+                throw new Error("unknown state");
+
+
+        }
+       currentLocation--;
+    }
+
+}
